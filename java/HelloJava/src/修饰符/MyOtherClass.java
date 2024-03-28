10dc7e8d60d758923875b810fc2459b6a793a54f
Zerone <1012978790@qq.com>
2024-03-28 23:46:41 +0800

    修饰符

---
+++ java/HelloJava/src/修饰符/MyOtherClass.java
@@ -0,0 +1,8 @@
+package 修饰符;
+
+class MyOtherClass {
+    public static void main(String[] args) {
+        MyClass obj = new MyClass(); 
+        obj.display(); // 访问 MyClass 中的默认访问修饰符变量和方法
+    }
+}
---
+++ java/HelloJava/src/修饰符/Myclass.java
@@ -0,0 +1,9 @@
+package 修饰符;
+
+class MyClass {
+    int x = 10;  // 默认访问修饰符
+ 
+    void display() {  // 默认访问修饰符
+        System.out.println("Value of x is: " + x);
+    }
+}
