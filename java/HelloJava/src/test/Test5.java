5e81da5e8ed34ee44616a9f7fdcc481087b437ea
Zerone <1012978790@qq.com>
2024-04-01 02:14:37 +0800

    练习2

--- java/HelloJava/src/test/Test5.java
+++ java/HelloJava/src/test/Test5.java
@@ -4,14 +4,22 @@ import java.util.Scanner;
 
 /**
  * 键盘录入一个正整数x，判断该整数是否为一个质数
- * 质数：如果一个整数只能被1和本身整除，这为质数
+ * 质数：如果一个整数只能被1和本身整除，则为质数
  */
 public class Test5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
-        for (int i = 1; i < x/2; i++){
-
+        boolean flag = true;
+        for (int i = 2; i < x/2; i++){
+            if(x % i == 0){
+                flag = false;
+                System.out.println("该数是合数");
+                break;
+            }
+        }
+        if (flag){
+            System.out.println("该数是质数");
         }
     }
 }
--- java/HelloJava/src/test/Test6.java
+++ java/HelloJava/src/test/Test6.java
@@ -1,2 +1,30 @@
-package test;public class Test6 {
+package test;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * 程序自动生成一个1-100之间的随机数字，
+ * 使用程序实现猜出这个数字的多少？
+ */
+public class Test6 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int number = random.nextInt(100) + 1;
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            int guessnumber= scanner.nextInt();
+            if (guessnumber > number){
+                System.out.println("大了");
+            }else if (guessnumber < number){
+                System.out.println("小了");
+            }else {
+                System.out.println("猜对了");
+                break;
+            }
+        }
+    }
+
+
+
 }
